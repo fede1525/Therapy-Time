b8dfe13361d1a6a797f1e29a636cdf7dd43f02f9
Federico Resano <123256603+fede1525@users.noreply.github.com>
2024-02-29 21:58:34 +0000

    Pull de develop

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -12,16 +12,16 @@ export const Login = () => {
         password: "",
     });
     const navigate = useNavigate();
-    
+
     useEffect(() => {
-        actions.getUsers(); 
+        actions.getUsers();
     }, []);
 
     const handleLogin = async (e) => {
         e.preventDefault();
         const errors = validateForm();
         setErrorMessages(errors);
-    
+
         if (Object.values(errors).every(error => error === "")) {
             try {
                 const result = await actions.loginUser(username, password);
@@ -32,11 +32,11 @@ export const Login = () => {
                 }
             } catch (error) {
                 console.error("Error en el inicio de sesión:", error.message);
-                setErrorMessage(prevState => ({ ...prevState, networkError: "Error de red" })); 
+                setErrorMessage(prevState => ({ ...prevState, networkError: "Error de red" }));
             }
         }
     };
-    
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         if (name === "username") {
@@ -55,7 +55,7 @@ export const Login = () => {
     };
 
     const validateForm = () => {
-        const errors = {}; 
+        const errors = {};
         if (username.trim() === "") {
             errors.username = "*El campo es obligatorio";
         } else {
@@ -64,22 +64,22 @@ export const Login = () => {
                 errors.username = "El usuario no está registrado";
             } else {
                 errors.username = "";
-            if (password.trim() === "") {
-                errors.password = "*El campo es obligatorio";
-            }
-            else {
-                errors.password=""
+                if (password.trim() === "") {
+                    errors.password = "*El campo es obligatorio";
+                }
+                else {
+                    errors.password = ""
                 }
             }
         }
         return errors;
     };
-    
+
     return (
         <div className="container login">
             <form onSubmit={handleLogin}>
                 <div className="form-group">
-                    <label htmlFor="username">Username:</label>
+                    <label htmlFor="username">Usuario :</label>
                     <input
                         type="text"
                         className="form-control"
@@ -93,7 +93,7 @@ export const Login = () => {
                     {errorMessages.username && <p className="text-danger">{errorMessages.username}</p>}
                 </div>
                 <div className="form-group">
-                    <label htmlFor="password">Password:</label>
+                    <label htmlFor="password">Contraseña :</label>
                     <input
                         type="password"
                         className="form-control"
@@ -116,16 +116,16 @@ export const Login = () => {
                 )}
                 <div className="text-center">
                     <button type="submit" className="btn btnLogin">
-                        Login
+                        Ingresar
                     </button>
                 </div>
             </form>
             <div className="mt-3 text-center link">
                 <p>
-                     Forgot your password? <Link to="/recovery">Recover it here</Link>
+                    ¿Olvidaste tu contraseña? <Link to="/recovery">Haz click aqui para recuperarla</Link>
                 </p>
                 <p>
-                    <Link to="/">← Go Back</Link>
+                    <Link to="/">← Volver</Link>
                 </p>
             </div>
         </div>
