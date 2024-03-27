4028280e1a7d95b80f6a24d577a109478e9c032e
MSMARGARA <142249109+MSMARGARA@users.noreply.github.com>
2024-03-27 02:09:55 +0000

    resolucion de conflictos

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -638,7 +638,54 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.error('Error: ', error);
 					return { success: false, error: error.message || 'Error al cargar las reservas' };
 				}
+			},
+			getReservationByID: async (id) =>{
+				try {
+					const response = await getActions().apiFetch(`/get_reservation_by_id/${id}`, 'GET');
+			
+					if (!response.ok) {
+						throw new Error('Error al traer la reserva');
+					}
+			
+					const responseData = await response.json();
+					const reservationsData = responseData.data; 
+					console.log(reservationsData); 
+			
+					setStore({ reservationByID: reservationsData });
+					return { success: true, message: responseData.message };
+				} catch (error) {
+					console.error('Error: ', error);
+					return { success: false, error: error.message || 'Error al traer la reserva' };
+				}
+			},
+			searchUserByDNI: async (dni) => {
+				try {
+					const response = await getActions().apiFetch(`/search_user/${dni}`, 'GET');
+			
+					if (!response.ok) {
+						throw new Error('Error al buscar usuario por DNI');
+					}
+			
+					const responseData = await response.json();
+			
+					if (responseData.error) {
+						throw new Error(responseData.error);
+					}
+			
+					const userData = responseData; // No necesitas responseData.data, ya que la respuesta ya contiene los datos del usuario.
+			
+					// Actualiza el store con los datos del usuario
+					setStore({ userByDNI: userData });
+			
+					console.log("Datos del usuario recibidos:", userData);
+			
+					return { success: true, message: "Usuario encontrado correctamente" };
+				} catch (error) {
+					console.error('Error al buscar usuario por DNI:', error.message);
+					return { success: false, error: error.message };
+				}
 			}
+			
 		}
 	}
 };
